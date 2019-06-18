7d05b6aec41007d28b54d9b5427d84b7c433a944
Administrator <jxoday@163.com>
2019-06-18 23:28:52 +0800

    自定义点击事件

--- app/src/main/java/com/example/advance/chapter1/recyclerview/HomeAdapter.java
+++ app/src/main/java/com/example/advance/chapter1/recyclerview/HomeAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import com.example.advance.R;
@@ -15,17 +16,51 @@ import java.util.List;
 /**
  * @author jinxin
  */
-public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> implements View.OnClickListener, View.OnLongClickListener {
 
     private Context mContext;
     private List<String> mList;
-    public HomeAdapter(Context mContext, List<String>mList) {
+
+    public HomeAdapter(Context mContext, List<String> mList) {
         this.mContext = mContext;
         this.mList = mList;
     }
 
+    public void removeData(int position) {
+        mList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    /**
+     * 定义条目点击事件
+     */
+    public interface OnItemClickListener {
+        /**
+         * 定义条目点击事件
+         *
+         * @param view     视图
+         * @param position 条目
+         */
+        void onItemClick(View view, int position);
+
+        /**
+         * 定义条目长按点击事件
+         *
+         * @param view     视图
+         * @param position 条目
+         */
+        void onItemLongClick(View view, int position);
+    }
+
+    private OnItemClickListener mOnItemClickListener;
+
+    public void setmOnItemClickListener(OnItemClickListener mOnItemClickListener) {
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
+
     /**
      * 加载条目布局
+     *
      * @param viewGroup
      * @param position
      * @return
@@ -33,28 +68,62 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
-        MyViewHolder holder = new MyViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_recycler, viewGroup, false));
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_recycler, viewGroup, false);
+        MyViewHolder holder = new MyViewHolder(view);
+
+        //点击事件的监听
+        view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
         return holder;
     }
 
     /**
      * 将视图与数据进行绑定
+     *
      * @param myViewHolder
      * @param position
      */
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
+        myViewHolder.itemView.setTag(position);
         myViewHolder.textView.setText(mList.get(position));
     }
+
+    /**
+     * 点击事件回调
+     * @param view 视图
+     */
+    @Override
+    public void onClick(View view) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemClick(view, (int) view.getTag());
+        }
+    }
+
+    /**
+     * 长按点击事件回调
+     * @param view
+     * @return
+     */
+    @Override
+    public boolean onLongClick(View view) {
+        if (mOnItemClickListener != null) {
+            mOnItemClickListener.onItemLongClick(view, (int) view.getTag());
+        }
+        return true;
+    }
+
 
     @Override
     public int getItemCount() {
         return mList.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView textView;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.tv_item);
--- app/src/main/java/com/example/advance/chapter1/recyclerview/RecyclerViewActivity.java
+++ app/src/main/java/com/example/advance/chapter1/recyclerview/RecyclerViewActivity.java
@@ -1,28 +1,78 @@
 package com.example.advance.chapter1.recyclerview;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.advance.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jinxin
  */
 public class RecyclerViewActivity extends AppCompatActivity {
 
+    private static final String TAG = "RecyclerViewActivity";
+    private HomeAdapter mHomeAdaper;
+    private List<String> mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
 
+
+        initData();
+        initView();
+
+
+    }
+
+    private void initData() {
+        mList=new ArrayList<String>();
+        for (int i = 1; i < 20; i++)
+        {
+            mList.add(i+"");
+        }
+    }
+
+    private void initView() {
         RecyclerView mRecyclerView = findViewById(R.id.id_recycler_view);
         //设置布局管理器
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         //设置item增加和删除时的动画
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mHomeAdaper=new HomeAdapter(this, mList);
+        mRecyclerView.setAdapter(mHomeAdaper);
 
+        mHomeAdaper.setmOnItemClickListener(new HomeAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(RecyclerViewActivity.this, "点击第" + (position + 1) + "条", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, final int position) {
+                //长按弹出对话框
+                new AlertDialog.Builder(RecyclerViewActivity.this)
+                        .setTitle("确认删除吗？")
+                        .setNegativeButton("取消",null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mHomeAdaper.removeData(position);
+                            }
+                        })
+                        .show();
+            }
+        });
     }
 }
