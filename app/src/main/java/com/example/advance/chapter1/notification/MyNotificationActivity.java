86556d67c8856e73e708c7bfb4d1432981247645
Administrator <jxoday@163.com>
2019-06-19 23:40:27 +0800

    初始Notification

---
+++ app/src/main/java/com/example/advance/chapter1/notification/MyNotificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.advance.chapter1.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.advance.R;
+
+public class MyNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_notification);
+    }
+}
